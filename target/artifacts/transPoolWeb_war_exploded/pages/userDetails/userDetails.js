98571bfa3e737a3a6dcd9e137f96f3b041c0dfbe
Ohad Bar <ohad165@gmail.com>
2020-07-22 17:34:26 +0300

    x

--- target/artifacts/transPoolWeb_war_exploded/pages/userDetails/userDetails.js
+++ target/artifacts/transPoolWeb_war_exploded/pages/userDetails/userDetails.js
@@ -7,6 +7,26 @@ app1.controller('userDetailsCtrl',[ '$scope', '$http', function($scope, $http) {
         {mapUniqueName: "Asia", totalStations: "2", totalRoutes: "8" ,
             totalMapTrips:"4", totalTripsRequests:"12"}
     ];
+    function init() {
+        $scope.totalMapsInTheSystem = {};
+
+        $scope.createUser = function()
+        {
+            $http({
+                method: 'GET',
+                url: 'http://localhost:8080/transPoolWeb_war_exploded/pages/userDetails/UserMapDetailsServlet',
+                headers: {'Content-Type': 'application/json'},
+                data:  $scope.user
+            }).success(function (data)
+            {
+                $scope.status=data;
+            });
+        };
+    }
+
+    init();
+
+
 
 } ]);
 
