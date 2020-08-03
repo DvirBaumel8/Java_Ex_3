f58b3e7b368a929ce8fefd7237d3c5181111bb2b
Ohad Bar <ohad165@gmail.com>
2020-08-03 17:33:40 +0300

    Merge branch 'master' of https://github.com/DvirBaumel8/Java_Ex_3
    # Conflicts:
    #	transPoolEngine/src/engine/dto/mapPage/TripRequestDto.java

--- transPoolWeb/src/manager/constans/Constants.java
+++ transPoolWeb/src/manager/constans/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final String USER_NAME = "userName";
     public static final String USER_TYPE = "userType";
     public static final String MAP_NAME = "mapName";
-    public static final String MAP_UPLOAD_NAME = "mapUploadName";
+    public static final String MAP_UPLOAD_NAME = "map_name";
     public static final String USER_NAME_ERROR = "username_error";
     public static final String USER_AMOUNT_TO_LOAD = "amountToLoad";
 
